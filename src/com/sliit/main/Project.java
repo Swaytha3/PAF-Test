93c8c0cc3af743d3ef386167e4aaecd223c2083f
Swaytha3 <swaytha3@gmail.com>
2021-02-19 19:54:29 +0530

    changes in project

--- src/com/sliit/main/Project.java
+++ src/com/sliit/main/Project.java
@@ -4,7 +4,7 @@ public class Project {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is my first project at SLIIT for 2021");
+		System.out.println("This is my first project at SLIIT for 2021..");
 
 	}
 
