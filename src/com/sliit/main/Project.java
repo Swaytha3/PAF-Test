f9b398cb360b5966e0a854b00428e0b7700e4bbc
Swaytha3 <swaytha3@gmail.com>
2021-02-19 19:49:15 +0530

    added java file named project

---
+++ src/com/sliit/main/Project.java
@@ -0,0 +1,11 @@
+package com.sliit.main;
+
+public class Project {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my first project at SLIIT for 2021");
+
+	}
+
+}
